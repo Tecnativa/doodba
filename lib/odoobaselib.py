11cf5b074a268d6f6eaa47cf030c4845da16187a
Jairo Llopis <yajo.sk8@gmail.com>
2017-01-24 09:58:11 +0100

    :ambulance: Fix IO error
    Since `yaml.load_all` is a generator, it needs an open file descriptor.

--- lib/odoobaselib.py
+++ lib/odoobaselib.py
@@ -12,16 +12,14 @@ CLEAN = os.environ.get("CLEAN") == "yes"
 
 def addons_config():
     """Load configurations from ``ADDONS_YAML`` into a dict."""
-    with open(ADDONS_YAML) as addons_file:
-        docs = yaml.load_all(addons_file)
-
     config = dict()
-    for doc in docs:
-        for repo, addons in doc.items():
-            if addons == "all":
-                config[repo] = addons
-            else:
-                config.setdefault(repo, list())
-                config[repo] += addons
+    with open(ADDONS_YAML) as addons_file:
+        for doc in yaml.load_all(addons_file):
+            for repo, addons in doc.items():
+                if addons == "all":
+                    config[repo] = addons
+                else:
+                    config.setdefault(repo, list())
+                    config[repo] += addons
 
     return config
