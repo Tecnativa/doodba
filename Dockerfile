Tecnativa/doodba

commit 1080ced6fe9fc1cb17f6942071a92de0afb59483

Reduce odoo user permissions
Now it can only read/execute /opt/odoo folder and below. Odoo user should not be able to change odoo source code, so an infected addon cannot change odoo's source code.

This might be a breaking change under some circumstances, given `~odoo` now expands to /home/odoo.

Dockerfile
@@ -2,7 +2,7 @@ FROM debian:8
 MAINTAINER Tecnativa <info@tecnativa.com>
 
 # Enable Odoo user and filestore
-RUN useradd -md /opt/odoo odoo \
+RUN useradd -md /home/odoo odoo \
     && mkdir -p /var/lib/odoo \
     && chown -R odoo:odoo /var/lib/odoo
 VOLUME ["/var/lib/odoo"]
@@ -33,10 +33,10 @@ ONBUILD ENV PGUSER="$PGUSER" \
             PGDATABASE="$PGDATABASE"
 ONBUILD ARG LOCAL_CUSTOM_DIR=./custom
 ONBUILD COPY $LOCAL_CUSTOM_DIR /opt/odoo/custom
-ONBUILD RUN chown -R odoo:odoo .
 # https://docs.python.org/2.7/library/logging.html#levels
 ONBUILD ARG LOG_LEVEL=INFO
 ONBUILD RUN ["/opt/odoo/common/build.sh"]
+ONBUILD RUN chown -R root:odoo . && chmod -R u+rwX,g+rX-w,o= .
 ONBUILD USER odoo
 
 ARG PYTHONOPTIMIZE=2

commit 07c5c8be77ce80d531216d7bee3b22b56a764cbd

Give write access to odoo in /opt/odoo/auto/addons, to allow lazy symlinking
Bug introduced by 1080ced6fe9fc1cb17f6942071a92de0afb59483.

Dockerfile
@@ -37,7 +37,9 @@ ONBUILD COPY $LOCAL_CUSTOM_DIR /opt/odoo/custom
 ONBUILD ARG LOG_LEVEL=INFO
 ONBUILD RUN ["/opt/odoo/common/build.sh"]
 ONBUILD RUN ln -s /opt/odoo/custom/ssh ~root/.ssh
-ONBUILD RUN chown -R root:odoo . && chmod -R u+rwX,g+rX-w,o= .
+ONBUILD RUN chown -R root:odoo . \
+    && chmod -R u+rwX,g+rX-w,o= . \
+    && chmod -R g+w auto/addons
 ONBUILD USER odoo
 
 ARG PYTHONOPTIMIZE=2