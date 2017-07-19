bd37229f01c0ba252bc67da4736d69f75ac71c17
Jairo Llopis <yajo.sk8@gmail.com>
2017-07-19 11:53:34 +0200

    fixup! [IMP] Initial docker-compose based CI

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,100 +12,122 @@ from os import environ
 from os.path import basename, dirname, join
 from subprocess import Popen
 
+# Common test utilities
+DIR = dirname(__file__)
+SCAFFOLDINGS_DIR = join(DIR, "scaffoldings")
+ODOO_PREFIX = ("odoo", "--stop-after-init")
+
 # Variable matrix
-ODOO_VERSIONS = (
+ODOO_VERSIONS = frozenset((
     "10.0",
     "9.0",
-    # TODO Add tests for 8.0, the problem is that it does not autocreate a DB
-)
-PG_VERSIONS = (
+    "8.0",
+))
+PG_VERSIONS = frozenset((
     "9.6",
-)
+))
 
 
-def matrix(compose_files, odoo=ODOO_VERSIONS, pg=PG_VERSIONS):
+def matrix(odoo=ODOO_VERSIONS, pg=PG_VERSIONS,
+           odoo_skip=frozenset(), pg_skip=frozenset()):
     """All possible combinations.
 
     We compute the variable matrix here instead of in ``.travis.yml`` because
     this generates faster builds, given the scripts found in ``hooks``
     directory are already multi-version-build aware.
     """
-    return product(
-        product(("COMPOSE_FILE",), compose_files),
-        product(("ODOO_MINOR",), odoo),
-        product(("DB_VERSION",), pg),
+    return map(
+        dict,
+        product(
+            product(("ODOO_MINOR",), odoo ^ odoo_skip),
+            product(("DB_VERSION",), pg ^ pg_skip),
+        )
     )
 
 
 class ScaffoldingLookupCase(unittest.TestCase):
-    def setUp(self):
-        super().setUp()
-        # Common tests to run in any odoo container
-        _odoo = ("odoo", "--stop-after-init")
-        self.commands = [
+    def compose_test(self, workdir, sub_env, *commands):
+        """Execute commands in a docker-compose environment.
+
+        :param workdir:
+            Path where the docker compose commands will be executed. It should
+            contain a valid ``docker-compose.yaml`` file.
+
+        :param dict sub_env:
+            Specific environment variables that will be appended to current
+            ones to execute the ``docker-compose`` tests.
+
+            You can set in this dict a ``COMPOSE_FILE`` key to choose different
+            docker-compose files in the same directory.
+
+        :param tuple()... commands:
+            List of commands to be tested in the odoo container.
+        """
+        full_env = dict(environ, **sub_env)
+        with self.subTest(PWD=workdir, **sub_env):
+            try:
+                build = Popen(
+                    ("docker-compose", "build"),
+                    cwd=workdir,
+                    env=full_env,
+                )
+                self.assertFalse(build.wait())
+                for command in commands:
+                    with self.subTest(command=command):
+                        run = Popen(
+                            ("docker-compose", "run", "--rm", "odoo") +
+                            command,
+                            cwd=workdir,
+                            env=full_env,
+                        )
+                        self.assertFalse(run.wait())
+            finally:
+                down = Popen(
+                    ("docker-compose", "down", "-v"),
+                    cwd=workdir,
+                    env=full_env,
+                )
+                self.assertFalse(down.wait())
+
+    def test_smallest(self):
+        """Tests for the smallest possible environment."""
+        common_commands = (
             # Must generate a configuration file
             ("test", "-f", "/opt/odoo/auto/odoo.conf"),
             # Must be able to install base addons
-            _odoo + ("--init", "base"),
+            ODOO_PREFIX + ("--init", "base"),
             # Must be able to test successfully base addons
             ("unittest", "base"),
-        ]
-
-    def docker_compose_tests(self, env_matrix):
-        """Execute a docker-compose environment.
-
-        :param generator env_matrix:
-            A list of key-value pairs that conform the specific environments
-            that should be tested. The current environment will be appended
-            to each of those to execute the ``docker-compose`` tests.
-
-            It is required that each sub-environment contains a
-            ``COMPOSE_FILE`` key with a full path to a valid Docker Compose
-            file.
-        """
-        for sub_env in env_matrix:
-            sub_env = dict(sub_env)
-            workdir = dirname(sub_env["COMPOSE_FILE"])
-            sub_env["COMPOSE_FILE"] = basename(sub_env["COMPOSE_FILE"])
-            full_env = dict(environ, **sub_env)
-            with self.subTest(**sub_env):
-                try:
-                    build = Popen(
-                        ("docker-compose", "build"),
-                        cwd=workdir,
-                        env=full_env,
-                    )
-                    self.assertFalse(build.wait())
-                    for cmd in self.commands:
-                        with self.subTest(cmd=cmd):
-                            run = Popen(
-                                ("docker-compose", "run", "--rm", "odoo") +
-                                cmd,
-                                cwd=workdir,
-                                env=full_env,
-                            )
-                            self.assertFalse(run.wait())
-                finally:
-                    down = Popen(
-                        ("docker-compose", "down", "-v"),
-                        cwd=workdir,
-                        env=full_env,
-                    )
-                    self.assertFalse(down.wait())
-
-    def test_scaffoldings(self):
-        """One test per ``./scaffoldings/*.yaml`` file."""
-        self.commands += [
-            # ``custom/build.d`` was properly executed
-            ("test", "-f", "/home/odoo/created-at-build"),
-            # ``custom/entrypoint.d`` was properly executed
-            ("test", "-f", "/home/odoo/created-at-entrypoint"),
-            # ``custom/conf.d`` was properly concatenated
-            ("grep", "test-conf", "/opt/odoo/auto/odoo.conf"),
-        ]
-        self.docker_compose_tests(
-            matrix(iglob(join(dirname(__file__), "scaffoldings", "*.yaml")))
         )
+        for sub_env in matrix():
+            if sub_env["ODOO_VERSION"] == "8.0":
+                commads = (
+                    # Odoo 8.0 does not autocreate the database
+                    ("psql", "-d", "postgres", "-c", "create database prod"),
+                ) + common_commands
+            else:
+                commads = common_commands
+            self.compose_test(
+                join(SCAFFOLDINGS_DIR, "smallest"), sub_env, **commads)
+
+    def test_dotd(self):
+        """Test environment with common ``*.d`` directories."""
+        for sub_env in matrix():
+            self.compose_test(
+                join(SCAFFOLDINGS_DIR, "dotd"), sub_env,
+                # ``custom/build.d`` was properly executed
+                ("test", "-f", "/home/odoo/created-at-build"),
+                # ``custom/entrypoint.d`` was properly executed
+                ("test", "-f", "/home/odoo/created-at-entrypoint"),
+                # ``custom/conf.d`` was properly concatenated
+                ("grep", "test-conf", "/opt/odoo/auto/odoo.conf"),
+                # ``dummy_addon`` and ``private_addon`` exist
+                ("test", "-d", "/opt/odoo/auto/addons/dummy_addon"),
+                ("test", "!", "-d", "/opt/odoo/auto/addons/private_addon"),
+                ("test", "-d", "/opt/odoo/common/src/private/dummy_addon"),
+                # ``odoo`` command works
+                ("odoo", "--help"),
+            )
 
     def test_main_scaffolding(self):
         """Test the official scaffolding."""
@@ -117,7 +139,11 @@ class ScaffoldingLookupCase(unittest.TestCase):
                 cwd=tmpdirname,
             )
             self.assertFalse(clone.wait())
-            # Setup the devel environment
+            # Create inverseproxy_shared network
+            net = Popen(
+                ("docker", "network", "create", "inverseproxy_shared")
+            )
+            self.assertFalse(net.wait())
             tmpdirname = join(tmpdirname, "docker-odoo-base")
             setup = Popen(
                 ("docker-compose", "-f", "setup-devel.yaml",
@@ -125,21 +151,19 @@ class ScaffoldingLookupCase(unittest.TestCase):
                 cwd=tmpdirname,
             )
             self.assertFalse(setup.wait())
-            self.docker_compose_tests(
-                matrix(
-                    starmap(
-                        join,
-                        product(
-                            (tmpdirname,),
-                            # Test all 3 official environments
-                            ("devel.yaml", "test.yaml", "prod.yaml")
-                        )
-                    ),
-                    # TODO Test all supported versions
-                    # The main scaffolding is only provided in latest version
-                    odoo=("10.0",)
-                ),
-            )
+            # TODO Test all supported versions
+            for sub_env in matrix(odoo={"10.0"}):
+                # Setup the devel environment
+                sub_env["COMPOSE_FILE"] = "setup-devel.yaml"
+                self.compose_test(tmpdirname, sub_env, ())
+                # Test all 3 official environments
+                for dcfile in ("devel", "test", "prod"):
+                    sub_env["COMPOSE_FILE"] = f"{dcfile}.yaml"
+                    self.compose_test(
+                        tmpdirname, sub_env,
+                        # ``odoo`` command works
+                        ("odoo", "--help"),
+                    )
 
 
 if __name__ == "__main__":
