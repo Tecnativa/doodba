8ddb14a6f16320e5bd68c264b22589f9797fb726
Jairo Llopis <Yajo@users.noreply.github.com>
2017-07-28 12:33:09 +0200

    [REF] New Installer class and subclasses (#57)
    * [REF] New Installer class and subclasses
    
    * This should make (un)installation and cleanup clearer and fix #53.
    * ☔ Add dependency tests

--- lib/odoobaselib/installer.py
+++ lib/odoobaselib/installer.py
@@ -1,116 +1,113 @@
 # -*- coding: utf-8 -*-
-
-import logging
-
-from odoobaselib import do_command
-
-INSTALLERS = ('apt', 'gem', 'npm', 'pip')
-CLEANERS = ('apt', 'gem', 'npm')
+from os.path import exists
+from subprocess import check_call
+from odoobaselib import logging
 
 
 class Installer(object):
-    """Install all sorts of things."""
-    _has_run = {}
+    """Base class to install packages with some package system."""
+    _cleanup_commands = []
+    _install_command = None
+    _remove_command = None
 
-    def __init__(self, handler_name, file_path):
-        self.handler_name = handler_name
+    def __init__(self, file_path):
         self.file_path = file_path
-        self.requirements = self._parse_requirements()
+        self._requirements = self.requirements()
 
-    def has_run(self, install_type, update=True):
-        has_run = self._has_run.get(install_type, False)
-        if update:
-            self._has_run[install_type] = True
-        return has_run
-
-    def install(self):
-        install_handler = '_install_%s' % self.handler_name
-        getattr(self, install_handler)()
-
-    def remove(self):
-        remove_handler = '_remove_%s' % self.handler_name
-        getattr(self, remove_handler)()
+    def _run_command(self, command):
+        logging.info("Executing: %s", command)
+        return check_call(command, shell=isinstance(command, str))
 
     def cleanup(self):
-        remove_handler = '_cleanup_%s' % self.handler_name
-        getattr(self, remove_handler)()
+        """Remove cache and other garbage produced by the installer engine."""
+        for command in self._cleanup_commands:
+            self._run_command(command)
 
-    def _cleanup_apt(self):
-        self._run_command(
-            'apt-get -y autoremove',
-        )
-        self._run_command(
-            'rm -Rf /var/lib/apt/lists/*',
-        )
+    def install(self):
+        """Install the requirements from the given file."""
+        if self._requirements:
+            self._run_command(self._install_command + self._requirements)
+        else:
+            logging.info("No installable requirements found in %s",
+                         self.file_path)
 
-    def _install_apt(self):
-        if not self.has_run('apt'):
-            self._run_command(
-                'apt-get update',
-            )
-        self._install_requirements([
-            'apt-get',
-            '-o', 'Dpkg::Options::="--force-confdef"',
-            '-o', 'Dpkg::Options::="--force-confold"',
-            '-y', '--no-install-recommends', 'install',
-        ],
-            split=False,
-            shell=True,
-        )
+    def remove(self):
+        """Uninstall the requirements from the given file."""
+        if not self._remove_command:
+            return
+        if self._requirements:
+            self._run_command(self._remove_command + self._requirements)
+        else:
+            logging.info("No removable requirements found in %s",
+                         self.file_path)
 
-    def _remove_apt(self):
-        for requirement in self.requirements:
-            self._run_command(
-                'apt-get purge -y %s' % requirement,
-            )
-
-    def _cleanup_gem(self):
-        self._run_command(
-            'rm -Rf ~/.gem /var/lib/gems/*/cache/',
-        )
-
-    def _install_gem(self):
-        self._install_requirements(
-            'gem install --no-rdoc --no-ri --no-update-sources',
-        )
-
-    def _cleanup_npm(self):
-        self._run_command(
-            'rm -Rf ~/.npm /tmp/*',
-        )
-
-    def _install_npm(self):
-        self._run_command(
-            'npm install -g',
-        )
-
-    def _install_pip(self):
-        self._install_requirements(
-            'pip --no-cache-dir install',
-        )
-
-    def _install_requirements(self, command, split=True, shell=False):
-        if split:
-            command = command.split()
-        for requirement in self.requirements:
-            self._run_command(
-                command + [requirement],
-                split=False,
-                shell=shell,
-            )
-
-    def _run_command(self, command, split=True, shell=False):
-        logging.info("Executing: %s", command)
-        return do_command(command, split, shell)
-
-    def _parse_requirements(self):
+    def requirements(self):
+        """Get a list of requirements from the given file."""
         requirements = []
-        with open(self.file_path, 'r') as fh:
-            for line in fh.read().splitlines():
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                requirements += [
-                    req.strip() for req in line.split() if req.strip()
-                ]
+        try:
+            with open(self.file_path, 'r') as fh:
+                for line in fh:
+                    line = line.strip()
+                    if not line or line.startswith('#'):
+                        continue
+                    requirements += line.split()
+        except IOError:
+            # No requirements file
+            pass
         return requirements
+
+
+class AptInstaller(Installer):
+    _cleanup_commands = [
+        ['apt-get', '-y', 'autoremove'],
+        'rm -Rf /var/lib/apt/lists/*',
+    ]
+    _install_command = [
+        'apt-get',
+        '-o', 'Dpkg::Options::=--force-confdef',
+        '-o', 'Dpkg::Options::=--force-confold',
+        '-y', '--no-install-recommends', 'install',
+    ]
+    _remove_command = ["apt-get", "purge", "-y"]
+
+    def _dirty(self):
+        return exists("/var/lib/apt/lists/lock")
+
+    def cleanup(self):
+        if self._dirty():
+            super(AptInstaller, self).cleanup()
+
+    def install(self):
+        if not self._dirty():
+            self._run_command(['apt-get', 'update'])
+        return super(AptInstaller, self).install()
+
+
+class GemInstaller(Installer):
+    _cleanup_commands = [
+        'rm -Rf ~/.gem /var/lib/gems/*/cache/',
+    ]
+    _install_command = [
+        "gem", "install", "--no-rdoc", "--no-ri", "--no-update-sources",
+    ]
+
+
+class NpmInstaller(Installer):
+    _cleanup_commands = ['rm -Rf ~/.npm /tmp/*']
+    _install_command = ["npm", "install", "-g"]
+
+
+class PipInstaller(Installer):
+    _install_command = ["pip", "install", "--no-cache-dir", "-r"]
+
+    def requirements(self):
+        """Pip will use its ``--requirements`` feature."""
+        return [self.file_path]
+
+
+INSTALLERS = {
+    "apt": AptInstaller,
+    "gem": GemInstaller,
+    "npm": NpmInstaller,
+    "pip": PipInstaller,
+}
