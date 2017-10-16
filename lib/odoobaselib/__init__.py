47509e774af06190ff87cc51a8e9bbb213282184
Jairo Llopis <yajo.sk8@gmail.com>
2017-10-16 13:58:35 +0200

    🚑 Fix odoo core addon taking priority over extra version
    #84 introduced a regression that violated [the addon priority table](https://github.com/Tecnativa/docker-odoo-base#optodoocustomsrcaddonsyaml). This fixes the regression and tests it.

--- lib/odoobaselib/__init__.py
+++ lib/odoobaselib/__init__.py
@@ -41,8 +41,7 @@ logging.root.setLevel(_log_level)
 def addons_config():
     """Yield addon name and path from ``ADDONS_YAML``."""
     config = dict()
-    private_done = False
-    core_done = False
+    special_missing = {PRIVATE, CORE}
     try:
         with open(ADDONS_YAML) as addons_file:
             for doc in yaml.load_all(addons_file):
@@ -53,11 +52,8 @@ def addons_config():
                     continue
                 # Flatten all sections in a single dict
                 for repo, addons in doc.items():
-                    if repo == PRIVATE:
-                        private_done = True
-                    elif repo == CORE:
-                        core_done = True
                     logging.debug("Processing %s repo", repo)
+                    special_missing.discard(repo)
                     for glob in addons:
                         logging.debug("Expanding glob %s", glob)
                         for addon in iglob(os.path.join(SRC_DIR, repo, glob)):
@@ -66,31 +62,27 @@ def addons_config():
                             config.setdefault(addon, set())
                             config[addon].add(repo)
     except IOError:
-        logging.debug('Could not find addons configuration yml.')
+        logging.debug('Could not find addons configuration yaml.')
     # By default, all private and core addons are enabled
-    if not private_done:
-        logging.debug("Auto-adding all private repo addons")
-        config.update({
-            os.path.basename(addon): {PRIVATE}
-            for addon in iglob(os.path.join(SRC_DIR, PRIVATE, "*"))
-        })
-    if not core_done:
-        logging.debug("Auto-adding all core repo addons")
-        config.update({
-            os.path.basename(addon): {CORE}
-            for addon in iglob(os.path.join(SRC_DIR, CORE, "*"))
-        })
+    for repo in special_missing:
+        logging.debug("Auto-adding all addons from %s", repo)
+        for addon in iglob(os.path.join(SRC_DIR, repo, "*")):
+            addon = os.path.basename(addon)
+            config.setdefault(addon, set())
+            config[addon].add(repo)
     logging.debug("Resulting configuration: %r", config)
     for addon, repos in config.items():
         # Private addons are most important
         if PRIVATE in repos:
             yield addon, PRIVATE
+            continue
         # Odoo core addons are least important
-        elif repos == {CORE}:
+        if repos == {CORE}:
             yield addon, CORE
+            continue
+        repos.discard(CORE)
         # Other addons fall in between
-        elif len(repos) != 1:
+        if len(repos) != 1:
             logging.error("Addon %s defined in several repos %s", addon, repos)
             raise Exception
-        else:
-            yield addon, repos.pop()
+        yield addon, repos.pop()
