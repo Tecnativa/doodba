2be84ff395945ecc8760d21aa3137192bb5f17f2
Jairo Llopis <yajo.sk8@gmail.com>
2018-01-24 10:17:08 +0000

    Test SMTP settings
    Use a new scaffolding that makes sure there's a preexisting database.

---
+++ tests/scaffoldings/settings/custom/scripts/test_smtp_settings.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python-odoo-shell
+try:
+    from odoo.tools import config
+except ImportError:
+    from openerp.tools import config
+
+
+assert config.get("email_from") == "test@example.com"
+assert config.get("smtp_password") is False
+assert config.get("smtp_port") == 1025
+assert config.get("smtp_server") == "mailhog"
+assert config.get("smtp_ssl") is False
+assert config.get("smtp_user") is False
