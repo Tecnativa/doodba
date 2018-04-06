2152f6c086b72e47e724c9099162b9c31cfa65e9
Jairo Llopis <yajo.sk8@gmail.com>
2018-04-06 12:45:06 +0100

    Drop PYTHONOPTIMIZE=2 hacks definitely
    Related to #132, fixes #21

--- bin/install.sh
+++ bin/install.sh
@@ -19,32 +19,16 @@ apt_deps="$apt_deps libldap2-dev libsasl2-dev"
 
 apt-get install -y --no-install-recommends $apt_deps
 
-# Install all dependencies that do not support PYTHONOPTIMIZE=2
-# TODO Remove when these get fixed:
-# https://github.com/Tecnativa/docker-odoo-base/issues/18
-
 # Download requirements file to be able to patch it
 curl -SLo /tmp/requirements.txt $reqs
 reqs=/tmp/requirements.txt
-# https://github.com/Tecnativa/docker-odoo-base/issues/21
+
 if [ "$ODOO_VERSION" == "8.0" ]; then
     # Packages already installed that conflict with others
     sed -ir 's/pyparsing|six/#\0/' $reqs
-    pip_deps="psutil==2.1.1 pydot==1.0.2 vobject==0.6.6"
     # Extra dependencies for Odoo at runtime
     apt-get install -y --no-install-recommends file
-elif [ "$ODOO_VERSION" == "9.0" ]; then
-    pip_deps="psutil==2.2.0 pydot==1.0.2 vobject==0.6.6"
-else
-    pip_deps="psutil==4.3.1 pydot==1.2.3"
 fi
-
-optimize="$PYTHONOPTIMIZE"
-if [ $PYTHONOPTIMIZE -gt 1 ]; then
-    export PYTHONOPTIMIZE=1
-fi
-pip install --no-cache-dir $pip_deps
-export PYTHONOPTIMIZE="$optimize"
 
 # Build and install Odoo dependencies with pip
 pip install --no-cache-dir --requirement $reqs
