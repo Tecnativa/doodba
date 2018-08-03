e78d251f10fa0daf953c514462aa28bcbf2da274
Jairo Llopis <yajo.sk8@gmail.com>
2018-08-03 11:20:26 +0100

    Unified logger
    This avoids polluting the root logger, which could be used for other things.

--- lib/odoobaselib/__init__.py
+++ lib/odoobaselib/__init__.py
@@ -32,7 +32,7 @@ if ODOO_VERSION < 10:
     MANIFESTS = MANIFESTS[1:]
 
 # Customize logging for build
-logging.root.name = "doodba"
+logger = logging.getLogger("doodba")
 _log_level = os.environ.get("LOG_LEVEL", "")
 if _log_level.isdigit():
     _log_level = int(_log_level)
@@ -40,9 +40,9 @@ elif _log_level in LOG_LEVELS:
     _log_level = getattr(logging, _log_level)
 else:
     if _log_level:
-        logging.warning("Wrong value in $LOG_LEVEL, falling back to INFO")
-    _log_level = logging.INFO
-logging.root.setLevel(_log_level)
+        logger.warning("Wrong value in $LOG_LEVEL, falling back to INFO")
+    _log_level = logger.info
+logger.setLevel(_log_level)
 
 
 class AddonsConfigError(Exception):
@@ -77,21 +77,21 @@ def addons_config(filtered=True, strict=False):
                 # Skip sections with ONLY and that don't match
                 elif any(os.environ.get(key) not in values
                          for key, values in doc.get("ONLY", dict()).items()):
-                    logging.debug("Skipping section with ONLY %s", doc["ONLY"])
+                    logger.debug("Skipping section with ONLY %s", doc["ONLY"])
                     continue
                 # Flatten all sections in a single dict
                 for repo, addons in doc.items():
                     if repo == "ONLY":
                         continue
-                    logging.debug("Processing %s repo", repo)
+                    logger.debug("Processing %s repo", repo)
                     special_missing.discard(repo)
                     for partial_glob in addons:
-                        logging.debug("Expanding glob %s", partial_glob)
+                        logger.debug("Expanding glob %s", partial_glob)
                         full_glob = os.path.join(SRC_DIR, repo, partial_glob)
                         found = glob(full_glob)
                         if not found:
                             missing_glob.add(full_glob)
-                            logging.debug(
+                            logger.debug(
                                 "Skipping unexpandable glob '%s'",
                                 full_glob)
                             continue
@@ -101,16 +101,16 @@ def addons_config(filtered=True, strict=False):
                             )
                             if not any(os.path.isfile(m) for m in manifests):
                                 missing_manifest.add(addon)
-                                logging.debug(
+                                logger.debug(
                                     "Skipping '%s' as it is not a valid Odoo "
                                     "module", addon)
                                 continue
-                            logging.debug("Registering addon %s", addon)
+                            logger.debug("Registering addon %s", addon)
                             addon = os.path.basename(addon)
                             config.setdefault(addon, set())
                             config[addon].add(repo)
     except IOError:
-        logging.debug('Could not find addons configuration yaml.')
+        logger.debug('Could not find addons configuration yaml.')
     # Fail now if running in strict mode
     if strict:
         error = []
@@ -126,12 +126,12 @@ def addons_config(filtered=True, strict=False):
             )
     # By default, all private and core addons are enabled
     for repo in special_missing:
-        logging.debug("Auto-adding all addons from %s", repo)
+        logger.debug("Auto-adding all addons from %s", repo)
         for addon in glob(os.path.join(SRC_DIR, repo, "*")):
             addon = os.path.basename(addon)
             config.setdefault(addon, set())
             config[addon].add(repo)
-    logging.debug("Resulting configuration: %r", config)
+    logger.debug("Resulting configuration: %r", config)
     for addon, repos in config.items():
         # Private addons are most important
         if PRIVATE in repos:
